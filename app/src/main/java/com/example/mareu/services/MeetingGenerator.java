1322bb09b1bcbf766a044092c6e35b76dc5a63ad
katych <katych1010@gmail.com>
2019-12-21 00:35:48 +0100

    Mareu

--- app/src/main/java/com/example/mareu/services/MeetingGenerator.java
+++ app/src/main/java/com/example/mareu/services/MeetingGenerator.java
@@ -13,9 +13,9 @@ public class MeetingGenerator {
 
     public static List<Meeting> meetingList = Arrays.asList(
 
-            new Meeting("Reunion1", RoomGenerator.generateRooms().get(0), "14h", "Nov 12,2020", " olivier@lamzone.com,paul@lamzone.com,sara@lamzone.com"),
-            new Meeting("Reunion2", RoomGenerator.generateRooms().get(1), "15h","Jan 13,2020",  "katy@lamzone.com,paul@lamzone.com,sara@lamzone.com"),
-            new Meeting("Reunion3", RoomGenerator.generateRooms().get(2), "16h", "Mar 14,2020", "katy@lamzone.com,paul@lamzone.com,sara@lamzone.com")
+            new Meeting("Reunion1", RoomGenerator.generateRooms().get(0), "10:00 AM", "Nov 12,2020", " olivier@lamzone.com,paul@lamzone.com"),
+            new Meeting("Reunion2", RoomGenerator.generateRooms().get(1), "03:00 PM","Jan 13,2020",  "katy@lamzone.com,paul@lamzone.com"),
+            new Meeting("Reunion3", RoomGenerator.generateRooms().get(2), "04:00 PM", "Mar 14,2020", "katy@lamzone.com,paul@lamzone.com")
     );
 
     static List<Meeting> generateMeetings() {
